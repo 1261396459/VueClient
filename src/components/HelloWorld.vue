333c429c798116e051dd6597333707b20f368ae7
Also_Easy <1261396459@qq.com>
2021-02-09 18:42:41 +0800

    大厅界面构建-第一遍

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <input v-model="msg"><button @click="setM">获取数据</button>
+  <p v-for="item in msgList">{{item.id + " say: " + item.msg}}</p>
   </div>
 </template>
 
@@ -88,10 +10,30 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '',
+      name: '',
+      id:'',
+      msgList:[]
     }
+  },
+  methods:{
+    setM(){
+      this.$socket.emit('chat message',this.msg)
+      this.msg=''
+    }
+  },
+  sockets:{
+    'connect':function(){
+      this.id = this.$socket.id;
+    },
+    'chat message':function(data){  //监听message事件，方法是后台定义和提供的
+        this.msgList.push(data)
+    }
+  },
+  mounted(){
+
   }
-}
+ }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
