c41806c9eab8f6927dd5efeab164a309361cde72
Also_Easy <1261396459@qq.com>
2021-02-09 18:45:51 +0800

    构建登录页-第一遍

---
+++ src/components/LogIn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input v-model="name"/>
+    <button @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'LogIn',
+    data:{
+      name:''
+    },
+    methods:{
+      login:()=>{
+        this.name=''
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
